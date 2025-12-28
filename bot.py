6407069ca347b8c55a3081f777345f69f522933d
stepkovichayu <stepkovich.iek@gmail.com>
2025-12-28 08:34:28 +0300

    SYMBOLS = ["1000PEPEUSDC", "SUIUSDC", "1000SHIBUSDC", "ENAUSDC"]

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ class Config:
     API_SECRET = os.getenv("BINANCE_API_SECRET", "")
 
     # ТОРГУЕМЫЕ ПАРЫ
-    SYMBOLS = ["1000PEPEUSDC", "SUIUSDC", "DOGEUSDC", "XRPUSDC"]
+    SYMBOLS = ["1000PEPEUSDC", "SUIUSDC", "1000SHIBUSDC", "ENAUSDC"]
 
     LEVERAGE = 10
 
