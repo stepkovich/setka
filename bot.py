e930e43ff1fde3bf4f867d6a1f9d81169b3e0bbc
stepkovichayu <stepkovich.iek@gmail.com>
2026-01-29 16:21:45 +0300

    +++  "ENAUSDC", "ADAUSDC"

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ class Config:
     API_KEY = os.getenv("BINANCE_API_KEY", "")
     API_SECRET = os.getenv("BINANCE_API_SECRET", "")
 
-    SYMBOLS = ["SUIUSDC","DOGEUSDC", "1000PEPEUSDC", "SOLUSDC", "XRPUSDC"]
+    SYMBOLS = ["SUIUSDC","DOGEUSDC", "1000PEPEUSDC", "SOLUSDC", "XRPUSDC", "ENAUSDC", "ADAUSDC"]
     LEVERAGE = 25
 
     # --- РИСК-МЕНЕДЖМЕНТ (Для 15 уровней и 25 плеча ---- 10) ---
