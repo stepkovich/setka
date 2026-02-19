958a2b7eda242db4da739ba69634f5a4f14b622f
stepkovichayu <stepkovich.iek@gmail.com>
2026-02-19 16:18:57 +0300

    mich 2

--- bot.py
+++ bot.py
@@ -37,23 +37,23 @@ class Config:
     API_KEY = os.getenv("BINANCE_API_KEY", "")
     API_SECRET = os.getenv("BINANCE_API_SECRET", "")
 
-    SYMBOLS = ["DOGEUSDC"]
-    LEVERAGE = 75
+    SYMBOLS = ["SUIUSDC", "DOGEUSDC", "1000PEPEUSDC", "XRPUSDC", "ENAUSDC"]
+    LEVERAGE = 25
 
     # --- РИСК-МЕНЕДЖМЕНТ ---
     # Расчет ордера от ДОСТУПНОГО БАЛАНСА
-    BALANCE_PER_1_DOLLAR_ORDER = Decimal("1")
+    BALANCE_PER_1_DOLLAR_ORDER = Decimal("6.5")
 
     # Максимальный множитель объема позиции (Защита Recon от перегруза)
     MAX_EXPOSURE_MULTIPLIER = Decimal("100.0")
 
     MIN_ORDER_SIZE = Decimal("5.2")
-    MAX_ORDER_SIZE = Decimal("5.2")
+    MAX_ORDER_SIZE = Decimal("25")
 
     GRID_LEVELS = 9
     FIB_STEP_BASE = Decimal("0.0002")
     VOL_COEFF = Decimal("120.0")
-    TAKE_PROFIT_PCT = Decimal("0.001")
+    TAKE_PROFIT_PCT = Decimal("0.002")
     PAGEN = 3
 
     STOP_LOSS_BEYOND_GRID_PCT = Decimal("0.015")
