78257b7941a367bcd3339b38d1a6a4ef61906b85
stepkovichayu <stepkovich.iek@gmail.com>
2026-04-03 09:58:57 +0300

    SYMBOLS = ["DOGEUSDC", "1000PEPEUSDC"]

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ class Config:
     API_KEY = os.getenv("BINANCE_API_KEY", "")
     API_SECRET = os.getenv("BINANCE_API_SECRET", "")
 
-    SYMBOLS = ["DOGEUSDC"]
+    SYMBOLS = ["DOGEUSDC", "1000PEPEUSDC"]
     LEVERAGE = 75
 
     # --- РИСК-МЕНЕДЖМЕНТ ---
